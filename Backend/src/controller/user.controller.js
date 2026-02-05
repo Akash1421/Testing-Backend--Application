569179a2dedd1e179ce420d9e90f39f20e4d99d5
Akash1421 <nadimpallyakash123@gmail.com>
2026-02-05 16:32:35 +0530

    fixed bug

--- Backend/src/controller/user.controller.js
+++ Backend/src/controller/user.controller.js
@@ -15,7 +15,14 @@ const registerUser = async (req, res) =>{
         if(existing){
             return res.status(400).json({message : "User already exists!"});
         }
+
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password,
+            loggedIn: false
+        });
     } catch (error) {
-        
+        res.status(500).json({message: "Internal server error",error})
     }
 };

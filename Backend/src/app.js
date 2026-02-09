ff4462ca11ca85fa7501eb7c54c8f5acf0682ff5
Akash1421 <nadimpallyakash123@gmail.com>
2026-02-09 17:48:15 +0530

    routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express(); //creates an express application
 
+//importing routes
+import userRoute from './routes/user.route.js'
+
+//implementing routes
+app.use("/api/v1/users", userRoute);
+
 export default app;
